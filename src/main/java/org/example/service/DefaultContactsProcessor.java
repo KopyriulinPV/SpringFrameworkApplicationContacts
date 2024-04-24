46b9b069af056039f1705be58c281bd6f2e24706
KPVL <kpv636363@mail.ru>
2024-04-24 11:34:57 +0400

    Initial commit

---
+++ src/main/java/org/example/service/DefaultContactsProcessor.java
@@ -0,0 +1,29 @@
+package org.example.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Profile("init")
+public class DefaultContactsProcessor {
+
+    @Value("${app.default-contacts-path}")
+    private String filePath;
+
+    private final FileReader fileReader;
+
+    private final UserService userService;
+
+    public DefaultContactsProcessor(FileReader fileReader, UserService userService) {
+        this.fileReader = fileReader;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void saveDefaultContacts() {
+        fileReader.readFileFrom(filePath).forEach(userService::save);
+    }
+}
